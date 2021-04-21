5b9faafb3b2a1efb421449dd25f06fee3e164352
davidxiong <daweix3@illinois.edu>
2021-04-21 22:09:42 +0800

    more on algorithm yet unfinished

--- src/algorithms/algorithm.h
+++ src/algorithms/algorithm.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "data/Point.h"
+#include "data/Road.h"
+#include "data/Map.h"
 
 class algorithm{
-    private:
     public:
+    void Dijkstra(Point* start, Point* destination);
+    void Astart(Point* start, Point* destination);
+    void Dstar(Point* start, Point* destination);
+
+    private:
+    Point* start_;
+    Point* destination_;
+
 
 
 }
