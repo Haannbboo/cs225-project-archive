b7a21a0a7e5e89986fc2b342f2213f1f6ff5b38d
Hanbo <hanbo@DESKTOP-TKJM3GK.localdomain>
2021-05-08 12:48:37 +0800

    makefile for tests completed

--- src/.objs/data/Road.d
+++
@@ -1,8 +0,0 @@
-.objs/data/Road.o: data/Road.cpp data/Road.h data/Point.h \
-  data/../DataLoader/dataLoader.h
-
-data/Road.h:
-
-data/Point.h:
-
-data/../DataLoader/dataLoader.h:
--- src/data/Point.cpp
+++ src/data/Point.cpp
@@ -17,5 +17,9 @@ bool Point::operator==(const Point &other) const{
 
 double Point::distance(Point* other) {
     // Euclidean distance
-    return sqrt((x*1000000 - other->x*1000000) * (x*1000000 - other->x*1000000) + (y*1000000 - other->y*1000000) * (y*1000000 - other->y*1000000));
+    // return sqrt((x*1000000 - other->x*1000000) * (x*1000000 - other->x*1000000) + (y*1000000 - other->y*1000000) * (y*1000000 - other->y*1000000));
+
+    // Great-Circle distance
+    double R = 6371000.0;  // radius of earth
+    return R * acos(cos(y) * cos(other->y) * cos(other->x - x) + sin(y) * sin(other->y));
 }
