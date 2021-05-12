26435f001e065fcec6eb719b49c4e6908e1cb08c
lurenw2 <lurenw2@illinois.edu>
2021-05-12 13:23:06 +0800

    push with issue on line 20

--- src/algorithms/Astar.h
+++ src/algorithms/Astar.h
@@ -1,42 +1,42 @@
-// #include <iostream>
-// #include <vector>
-// #include "../data/Point.h"
-// #include "../data/Road.h"
-// #include "../data/Map.h"
-// #include <list>
+#include <iostream>
+#include <vector>
+#include "../data/Point.h"
+#include "../data/Road.h"
+#include "../data/Map.h"
+#include <list>
 
 
-// #include <vector> 
+#include <vector> 
 
-// #include <list> 
+#include <list> 
 
-// class Astar {
-//     public:
-//     Astar();
-//     Astar(Point* start, Point* des, std::string filename);
-//     std::vector<Road*> getSurroundPoints(Point* point);
-//     double calcG(Point* A, Road* R);
-//     double calcH(Point* A);
-//     double calcF(Road* road, Point* point);
-//     Point* getLeastFpoint();
-//     bool isInList(const Point* point) const;
-//     Point* isInList() const;
-//     Point* findPath();
-//     void getPath();
-//     void print_path(Point* point);
+class Astar {
+    public:
+    Astar();
+    Astar(Point* start, Point* des, std::string filename);
+    std::vector<Road*> getSurroundPoints(Point* point);
+    double calcG(Point* A, Road* R);
+    double calcH(Point* A);
+    double calcF(Road* road, Point* point);
+    Point* getLeastFpoint();
+    bool isInList(const Point* point) const;
+    Point* isInList() const;
+    Point* findPath();
+    void getPath();
+    void print_path(Point* point);
 
-//     // FUNCTIONS FOR TESTING
-//     void ToOpenList(Point* point);
-//     void ToCloseList(Point* point);
-//     void ToPath(Point* point);
-//     Map* map_;
+    // FUNCTIONS FOR TESTING
+    void ToOpenList(Point* point);
+    void ToCloseList(Point* point);
+    void ToPath(Point* point);
+    Map* map_;
 
-//     private:
-//     std::list<Point*> openList; 
-//     std::list<Point*> closeList;
-//     std::list<Point*> path_;
+    private:
+    std::list<Point*> openList; 
+    std::list<Point*> closeList;
+    std::list<Point*> path_;
 
    
-//     Point* start_;
-//     Point* destination_;
-// };   
+    Point* start_;
+    Point* destination_;
+};   
