c578cb74752055f6fd5ba898852f260dca0f5b24
lurenw2 <lurenw2@illinois.edu>
2021-05-13 09:36:03 +0800

    finish Dij

--- src/algorithms/Dijkstra.cpp
+++ src/algorithms/Dijkstra.cpp
@@ -26,6 +26,7 @@
     visited_points.push_back(cur);
     while(cur != destination){
         cur = findnearstpoint();
+        cur->visited = true;
         // currentpoint->visited = true;
         visited_points.push_back(cur);
         road.push_back(cur);
@@ -37,19 +38,20 @@ Point* Dijkstra::findnearstpoint(){
     double min = 99999999; //minimum dis counter
     Point* minp; //the point with minimum dis
     for(auto visit: visited_points){ //loop over visited points and get connected points
-        getpoints(visit);
+        getpoints(visit); //update unvisited neighbout points
         for(auto point:visit->connectedpts){ //loop over connected points
-            double dis = point->distance(visit);
-            if(point->best_estimate == 999999){ //if not updated before, update
-                point->best_estimate = dis;
-            } else if(visit->best_estimate+dis < point->best_estimate){ //if smaller than current estimate, update
+            if(point->visited == false){
+                double dis = point->distance(visit); //calculate the distance
+                if(visit->best_estimate+dis < point->best_estimate){ //if smaller than current estimate, update
                 point->best_estimate = visit->best_estimate+dis;
-            }
-            if(point->best_estimate < min){ // find out the smallest point to take as nearest point
+                }
+                if(point->best_estimate < min){ // find out the smallest point to take as nearest point
                 //cout<<"best estimate: "<<point->best_estimate<<endl;
-                min = point->best_estimate;
-                minp = point;
+                    min = point->best_estimate;
+                    minp = point;
+                }
             }
+            
         }
     }
     //cout<<minp<<endl;
@@ -58,35 +60,24 @@ Point* Dijkstra::findnearstpoint(){
     //   cout<<point<<endl;
     //}
     //cout<<"finish findnearest point"<<endl;
+    cout<<minp;
     return minp;
 }
 
 void Dijkstra::getpoints(Point * a) { //add all unvisited point to a's connected point group
+    a->connectedpts.clear();
     vector<Road*> roads = map_->incidentRoads(a);
     for(auto road:roads){
         Point * otherside = road->otherSide(a);
-        bool flag = true;
-        for(auto point:visited_points){ //check if the point is visited before
-            if(point == otherside)
-                flag = false;
-                for(size_t i = 0; i < a->connectedpts.size(); i++){
-                    if(a->connectedpts[i] == otherside)
-                        a->connectedpts.erase(a->connectedpts.begin() + i - 1); //if this point is visited before, 
-                                                                                //erase this point from the connected points
-                }
-        }
-        if(flag == true) //if the point is not visited before, then add
-            for(auto point:a->connectedpts){
-                if(otherside == point)
-                    flag = false;
-            }
-        if(flag == true)
+        if(otherside->visited == false){
             a->connectedpts.push_back(otherside);
+        }
+
         
     }
 }
 void Dijkstra::print_path(){
-    cout<<"the path should be:"<<endl;
+    cout<<"the estimate should be:"<<endl;
     for(size_t i = 0; i < road.size(); i++){
         cout<<"("<<road[i]->x<<","<<road[i]->y<<") "<<road[i]->best_estimate<<" "<<endl;
     }
