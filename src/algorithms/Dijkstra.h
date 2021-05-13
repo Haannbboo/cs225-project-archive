9e883edf81b9326eb5a281de69ae1a1e951c5789
lurenw2 <lurenw2@illinois.edu>
2021-05-13 12:11:06 +0800

    push final

--- src/algorithms/Dijkstra.h
+++ src/algorithms/Dijkstra.h
@@ -12,27 +12,22 @@ const int kCost2=14;
 
 class Dijkstra{
     public:
-    //void InitAstar(std::vector<std::vector<int>> &_maze); 
     Dijkstra();
     Dijkstra(Point* start,Point* destination, string filename);
     Dijkstra(Point* a, Point* b,  Map* m);
-    //void MazeToAdjMatrix(std::vector<std::vector<int>> &_maze);
     Point* findnearstpoint(); //find and return the nearst point to a point
     double distance(Point* a, Point* b); //the distance between two points
     void print_path();
     void getPath();
     void getpoints(Point *a);
-    private:
+    vector<Point*> findpath();
+    Map* map_;
     vector<Point*> road;
+    private:
+    
     Point* start_;
     Point* destination_;
     std::vector<Point*> path_;
-    Map* map_;
     vector<Point*> visited_points;
-    int n; //total number of vertices
-    std::vector<Point*> dis; // store the shortest path from start to other points;
-    //std::vector<NewPoint *> getSurroundPoints( const NewPoint *point, bool isIgnoreCorner) const;
-    //bool isCanreach( const NewPoint *point, const NewPoint *target, bool isIgnoreCorner) const 
-    
 
 };
